8eb1bf3e502e073fb04aebbad7c8f43b5a98f67c
Keian <KeianAnthony1@gmail.com>
2021-05-30 11:50:17 -0700

    Added withAuth0 to Header.js

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
 import LogoutButton from './LogoutButton';
 import LoginButton from './LoginButton';
+import { withAuth0 } from '@auth0/auth0-react';
 // import './Header.css';
 
 class Header extends React.Component {
@@ -13,16 +14,19 @@ class Header extends React.Component {
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
           <Navbar.Brand>My Favorite Books</Navbar.Brand>
           {/* WE WILL CHANGE THESE ONCE HOMEPAGE IS POPULATED WITH IMAGES LATER */}
-          <Link to="/">Homepage</Link>
-          <Link to="/profile">Profile</Link>
-          <Link to="/pokemon">Pokemon</Link>
-          <Link to="/knowledge">Knowledge</Link>
+          {this.props.auth0.isAuthenticated
+            ? <>
+              <Link to="/">Homepage</Link>
+              <Link to="/profile">Profile</Link>
+              <Link to="/pokemon">Pokemon</Link>
+              <Link to="/knowledge">Knowledge</Link>
+            </>
+            : null}
           {this.props.isAuthenticated ? <LogoutButton /> : <LoginButton />}
         </Navbar>
-
       </div>
     );
   }
 }
 
-export default Header;
+export default withAuth0(Header);
