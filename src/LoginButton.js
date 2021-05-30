e29aaaefcf3dafd675d98b8f68d36ddc88f7abcf
Keian <KeianAnthony1@gmail.com>
2021-05-30 14:42:23 -0700

    Got login/logout button working on header, more changes to knowledge.js game, general data populated to Profile.js based on the logged in user

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './Login.css';
 import { useAuth0 } from '@auth0/auth0-react';
 
--- src/Profile.js
+++ src/Profile.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 import React from 'react';
+import { withAuth0 } from '@auth0/auth0-react';
 
 class Profile extends React.Component {
   constructor(props) {
@@ -10,9 +11,14 @@ class Profile extends React.Component {
     return (
       <>
         <h1>You are on the Profile page</h1>
+        <div className="profileData">
+          <h2>Username: {this.props.auth0.user.nickname}</h2>
+          <img src={this.props.auth0.user.picture} alt="User Picture" />
+          <h3>Email: {this.props.auth0.user.email}</h3>
+        </div>
       </>
     )
   }
 }
 
-export default Profile;
+export default withAuth0(Profile);
