c8a779d0a08134897d303ed902e0f7b1f12fcf9d
Keian <KeianAnthony1@gmail.com>
2021-05-30 10:31:41 -0700

    added routes for different components for the App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
+import { Route, BrowserRouter } from 'react-router-dom';
+import IsLoadingAndError from './IsLoadingAndError';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <BrowserRouter>
+        <IsLoadingAndError>
+          {/* Need to make Header component */}
+          <Header />
+          <Route exact path="/">
+            {this.props.auth0.isAuthenticated
+              // Need to make HomePage component
+              ? <HomePage />
+              : null}
+          </Route>
+          <Route exact path="/profile">
+            {this.props.auth0.isAuthenticated
+              // Need to make Profile component
+              ? <Profile />
+              : null}
+          </Route>
+          <Route exact path="/knowledge">
+            {this.props.auth0.isAuthenticated
+              // Need to make Knowledge component
+              ? <Knowledge />
+              : null}
+          </Route>
+          <Route exact path="/pokemon">
+            {this.props.auth0.isAuthenticated
+              // Need to make Pokemon component
+              ? <Pokemon />
+              : null}
+          </Route>
+        </IsLoadingAndError>
+      </BrowserRouter>
+    </>
   );
 }
 
