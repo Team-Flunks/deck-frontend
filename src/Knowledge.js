3659e4ca025227512e288f385b3782c73e52c1d2
Peyton Mader <middpeyton@gmail.com>
2021-06-01 12:24:38 -0700

    Add high score functionality and create end of game modal

--- src/Knowledge.js
+++ src/Knowledge.js
@@ -3,6 +3,7 @@ import React from 'react';
 import axios from 'axios';
 import { withAuth0 } from '@auth0/auth0-react';
 import Background from './Background.js'
+import KnowledgeEndModal from './KnowledgeEndModal.js'
 import './css/Knowledge.css';
 import { Modal } from 'react-bootstrap';
 
@@ -14,15 +15,47 @@ class Knowledge extends React.Component {
       currentStatement: '',
       currentStatementCandor: null,
       score: 0,
-      rounds: 1,
+      rounds: 0,
+      highScore: 0,
       selectedButton: null,
       gameState: true,
       showModal: false,
+      showEndModal: false,
+      didWin: false,
     }
   }
 
   componentDidMount() {
+    this.getRecord();
     this.fetchStatement();
+  }
+
+//Get past high scores
+  getRecord = () => {
+    this.props.auth0.getIdTokenClaims()
+      .then(tokenData => {
+        const jwt = tokenData.__raw;
+        const requestConfig = {
+          headers: { "Authorization": `Bearer ${jwt}` },
+          method: 'get',
+          baseURL: process.env.REACT_APP_SERVER_URL || 'http://localhost:3002',
+          url: `/singleUser`
+        }
+        axios(requestConfig)
+          .then(response => {
+            let array = response.data.gameRecords;
+
+            console.log(array)
+            const singleGame = array.filter(game => game.game === "knowledgegame");
+            console.log(singleGame)
+
+            console.log(singleGame[0].highscore)
+
+
+            this.setState({ highScore: singleGame[0].highscore });
+          })
+          .catch(err => console.error(err));
+      })
   }
 
   fetchStatement = () => {
@@ -46,29 +79,37 @@ class Knowledge extends React.Component {
   }
 
 
+// When the game is over, send the game name, if they won, and the score. Get back times won, high score
+// Need to do an inital get of records when they enter page
 
+  sendGameData = () => {
 
-  // sendGameData = () => {
-  //   this.props.auth0.getIdTokenClaims()
-  //     .then(tokenData => {
-  //       const jwt = tokenData.__raw;
-  //       const requestConfig = {
-  //         headers: { "Authorization": `Bearer ${jwt}` },
-  //         method: 'put',
-  //         baseURL: process.env.REACT_APP_SERVER_URL || 'http://localhost:3002',
-  //         url: `/knowledgegame/?currentCandor=${this.state.currentStatementCandor}`
-  //       }
-  //       axios(requestConfig)
-  //         .then(response => {
-  //           this.setState({ currentStatement: response.data });
-  //         })
-  //         .catch(err => console.error(err));
-  //     })
-  // }
+    this.props.auth0.getIdTokenClaims()
+      .then(tokenData => {
+        const jwt = tokenData.__raw;
+        const requestConfig = {
+          headers: { "Authorization": `Bearer ${jwt}` },
+          method: 'put',
+          baseURL: process.env.REACT_APP_SERVER_URL || 'http://localhost:3002',
+          url: `/updateUser/?game=knowledgegame&didWin=${this.state.didWin}&score=${this.state.score}`
+        }
+        axios(requestConfig)
+          .then(response => {
 
-  // ? game = string of game didWin = true score= number 
+            let array = response.data;
+            console.log(array)
 
+            const singleGame = array.filter(game => game.game === "knowledgegame");
+            console.log(singleGame)
 
+            this.setState({ highScore: singleGame[0].highscore });
+
+            this.setState({ score: 0 })
+            this.setState({ rounds: 0 })
+          })
+          .catch(err => console.error(err));
+      })
+  }
 
 
   testAnswer = () => {
@@ -76,8 +117,8 @@ class Knowledge extends React.Component {
       this.setState({ score: this.state.score + 1 });
     }
     this.setState({ rounds: this.state.rounds + 1 });
-    if (this.state.rounds === 10) {
-      this.setState({ gameState: false });
+    if (this.state.rounds === 9) {
+      this.handleEnd();
     }
     if (this.state.gameState) {
       this.fetchStatement();
@@ -90,6 +131,23 @@ class Knowledge extends React.Component {
     this.testAnswer();
   }
 
+  handleEnd = () => {
+    this.setState({showEndModal: true});
+
+    if (this.state.score > 7){
+      this.setState({didWin: true});
+    } else {
+      this.setState({didWin: false});
+    }
+
+    this.sendGameData();
+
+  }
+
+  setEndModal = (value) => {
+    this.setState({showEndModal: false})
+  }
+
   render() {
     return (
       <>
@@ -100,8 +158,8 @@ class Knowledge extends React.Component {
           <h1>The Knowledge Game</h1>
 
           <div className="gameStats">
-            <h4>High score: (tbd)</h4>
-            <h4>Current Score: {this.state.score}</h4>
+            <h4>High score: {this.state.highScore} / 10</h4>
+            <h4>Current Score: {this.state.score} / {this.state.rounds}</h4>
 
             <div id="instructions">
               <button id="instructionz" onClick={() => this.setState({ showModal: true })}>Instructions</button>
@@ -120,17 +178,19 @@ class Knowledge extends React.Component {
               </Modal.Header>
               <Modal.Body>
                 <p>
-                  You will be faced with 10 statements that are either truths, or lies. You have to figure out which is which! You get ten rounds, and if you get more than 5 correct... We'll count that as a win :)
+                  You will be faced with 10 statements that are either truths, or lies. You have to figure out which is which! You get ten rounds, and if you get more than 8 correct... We'll count that as a win :)
                   </p>
               </Modal.Body>
             </Modal>
 
+            <KnowledgeEndModal showEndModal={this.state.showEndModal} score={this.state.score} callback={this.setEndModal}/>
+
           </div>
 
-
-          {!this.state.gameState
-            ? <h2>Thanks for playing! You {this.state.score >= 8 ? "did" : "did not"} get a token!</h2>
-            : null}
+          {/* {!this.state.gameState
+            ?     
+            <KnowledgeEndModal showEndModal={this.state.showEndModal} score={this.state.score} callback={this.setEndModal}/>
+            : null} */}
           <div id="statement">
             <h3>Statement Will Be Here{this.state.currentStatement}</h3>
           </div>
