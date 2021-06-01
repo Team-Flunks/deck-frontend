df0f90228c9567805e61f32a5b324035bb341b86
Charles Bofferding <chaboffe@gmail.com>
2021-06-01 13:53:18 -0700

    Test Suite for back-end testing

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Knowledge from './Knowledge.js';
 import Pokemon from './Pokemon.js';
 import AboutUs from './AboutUs.js';
 import EnterPage from './EnterPage.js';
+import TestSuite from './TestSuite.js';
 import { withAuth0 } from '@auth0/auth0-react';
 import React from 'react';
 // import { TranitionGroup, CSSTransition } from 'react-transition-group';
@@ -20,6 +21,8 @@ class App extends React.Component {
           <IsLoadingAndError>
             {/* KEIAN I added a route for the home page which is home- and changed the / route for when someone first visits the page- they will see our enter screen! Feel free to change it back if you don't like it or if it messes up other things. I didn't spend a whole bunch of time on it. */}
             <Header />
+            {/* CHAZ Back end REALLY wants this to still be here, you can comment out the render but please don't delete the test suite */}
+            {/* <TestSuite /> */}
             <Route exact path="/">
               <EnterPage />
             </Route>
